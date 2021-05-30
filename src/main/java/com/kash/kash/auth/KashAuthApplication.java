70da6e3f3a11a17e65e5213e96fa47cfe2ab410a
Joel George <joelngeorge@outlook.com>
2021-05-30 16:29:01 +0530

    Initial commit

---
+++ src/main/java/com/kash/kash/auth/KashAuthApplication.java
@@ -0,0 +1,13 @@
+package com.kash.kash.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KashAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KashAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kash/kash/auth/KashAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kash.kash.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KashAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
